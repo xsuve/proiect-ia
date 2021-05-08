f4b591c248f56f6d3b9c5ec9948b60f8a61d2352
George Babă <georgebaba99@yahoo.com>
2021-05-08 13:34:52 +0300

    Imagini piese

--- Bishop.cs
+++ Bishop.cs
@@ -9,7 +9,7 @@ namespace Proiect_IA {
     internal class Bishop : Piece {
         public Bishop(Color color) :base(color) {
             image = color == Color.White ? Proiect_IA.Properties.Resources.bishop_w : Proiect_IA.Properties.Resources.bishop_b;
-
+            //
         }
     }
 }
